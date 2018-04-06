f74b5f0385657243727da1a00d7d9a5e3bda2d04
MartinStevko <mstevko10@gmail.com>
2018-04-06 10:10:25 +0200

    Database models creation

---
+++ eshop/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+from django.utils import timezone
+
+class Division(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+class Category(models.Model):
+    idDivision = models.ForeignKey(Division, on_delete=models.PROTECT)
+
+    name = models.CharField(max_length=30, unique=True)
+    image = models.ImageField()
+
+class Product(models.Model):
+    idCategory = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    specification = models.TextField()
+
+    in_storage = models.BooleanField(default=False)
+    is_public = models.BooleanField(default=True)
+
+class Galery(models.Model):
+    idProduct = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+    name = models.CharField(max_length=50, unique=True)
+    image = models.ImageField()
+
+class Review(models.Model):
+    idProduct = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+    author = models.CharField(max_length=50, unique=False)
+    email = models.EmailField(max_length=250)
+
+    text = models.TextField()
+    time = models.DateTimeField(default=timezone.now())
