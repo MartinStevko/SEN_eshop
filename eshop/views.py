e74b61eacabf7b2faf2b3f64dea602fad0c4df6e
MartinStevko <mstevko10@gmail.com>
2018-04-06 10:21:27 +0200

    Views creation

--- eshop/views.py
+++ eshop/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 
-# Create your views here.
+from .models import *
+
+def index(request):
+    pass
+
+def contact_us(request):
+    pass
+
+def categories(request):
+    pass
+
+def products(request):
+    pass
+
+def product_view(request, number):
+    pass
+
+def basket(request):
+    pass
+
+def order(request):
+    pass
