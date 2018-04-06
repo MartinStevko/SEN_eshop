54dd879eb2629c8bed7c4a45fb0baf720ae30185
MartinStevko <mstevko10@gmail.com>
2018-04-06 11:12:15 +0200

    Contact us form and view

--- eshop/views.py
+++ eshop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+
 from django.utils import timezone
+from django.core.mail import send_mail
 
 from .models import *
 
@@ -12,7 +14,39 @@ def index(request):
     return render(request, template, {'divisions':divisions, 'categories':categories})
 
 def contact_us(request):
-    pass
+    template = 'eshop/contact_us.html'
+
+    divisions = Division.objects.all()
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        # subject not null (html)
+        subject = request.POST['subject']
+        mail_text = request.POST['text']
+
+        try:
+            from_mail = request.POST['from']
+        except():
+            message = 'Zadajte správnu emailovú adresu!'
+            return render(request, template, {'divisions':divisions, 'categories':categories, 'message':message})
+
+        try:
+            from_mail = str(from_mail)
+        except():
+            message = 'Zadajte správnu emailovú adresu!'
+            return render(request, template, {'divisions':divisions, 'categories':categories, 'message':message})
+
+        try:
+            name = str(request.POST['name'])
+        except():
+            message = 'Zadajte vaše meno!'
+            return render(request, template, {'divisions':divisions, 'categories':categories, 'message':message})
+
+        text = 'E-mail from: ' + name + '\n \n' + 'Text: ' + str(mail_text)
+
+        send_mail(subject, text, from_mail, ['mstevko10@gmail.com', 'matkon1999@gmail.com'], fail_silently=False)
+    else:
+        return render(request, template, {'divisions':divisions, 'categories':categories})
 
 def categories(request):
     template = 'eshop/categories.html'
