7a7735994b78e87c32bf4d82773c3709490f697a
MartinStevko <mstevko10@gmail.com>
2018-04-14 19:01:16 +0200

    Navbar menu

--- eshop/views.py
+++ eshop/views.py
@@ -29,8 +29,14 @@ def nothing(request):
 def contact_us(request):
     template = 'eshop/contact_us.html'
 
-    divisions_menu = Division.objects.all()
-    categories_menu = Category.objects.all()
+    menu = []
+    divisions = Division.objects.all()
+    for divis in divisions:
+        temp = []
+        categories = Category.objects.filter(idDivision=divis)
+        temp.append(divis)
+        temp.append(categories)
+        menu.append(temp)
 
     if request.method == 'POST':
         # subject not null (html)
@@ -41,41 +47,53 @@ def contact_us(request):
             from_mail = request.POST['from']
         except():
             message = 'Zadajte správnu emailovú adresu!'
-            return render(request, template, {'divisions':divisions, 'categories':categories, 'message':message})
+            return render(request, template, {'menu':menu, 'message':message})
 
         try:
             from_mail = str(from_mail)
         except():
             message = 'Zadajte správnu emailovú adresu!'
-            return render(request, template, {'divisions':divisions, 'categories':categories, 'message':message})
+            return render(request, template, {'menu':menu, 'message':message})
 
         try:
             name = str(request.POST['name'])
         except():
             message = 'Zadajte vaše meno!'
-            return render(request, template, {'divisions':divisions, 'categories':categories, 'message':message})
+            return render(request, template, {'menu':menu, 'message':message})
 
         text = 'E-mail from: ' + name + '\n \n' + 'Text: ' + str(mail_text)
 
         send_mail(subject, text, from_mail, ['mstevko10@gmail.com', 'matkon1999@gmail.com'], fail_silently=False)
     else:
-        return render(request, template, {'divisions_menu':divisions_menu, 'categories_menu':categories_menu})
+        return render(request, template, {'menu':menu})
 
 def categories(request, division_id):
     template = 'eshop/categories.html'
 
-    divisions_menu = Division.objects.all()
-    categories_menu = Category.objects.all()
+    menu = []
+    divisions = Division.objects.all()
+    for divis in divisions:
+        temp = []
+        categories = Category.objects.filter(idDivision=divis)
+        temp.append(divis)
+        temp.append(categories)
+        menu.append(temp)
 
     categories = Category.objects.filter(idDivision=division_id)
 
-    return render(request, template, {'categories':categories, 'divisions_menu':divisions_menu, 'categories_menu':categories_menu})
+    return render(request, template, {'categories':categories, 'menu':menu})
 
 def products(request, category_id):
     template = 'eshop/products.html'
 
-    divisions_menu = Division.objects.all()
-    categories_menu = Category.objects.all()
+    menu = []
+    divisions = Division.objects.all()
+    for divis in divisions:
+        temp = []
+        categories = Category.objects.filter(idDivision=divis)
+        temp.append(divis)
+        temp.append(categories)
+        menu.append(temp)
 
     try:
         category = Category.objects.get(pk=category_id)
@@ -84,7 +102,7 @@ def products(request, category_id):
         mes = 'Kategória produktov neexistuje!'
         return redirect(index, message=mes)
 
-    return render(request, template, {'products':products, 'category':category, 'divisions_menu':divisions_menu, 'categories_menu':categories_menu})
+    return render(request, template, {'products':products, 'category':category, 'menu':menu})
 
 def product_view(request, product_id):
     the_product = ''
