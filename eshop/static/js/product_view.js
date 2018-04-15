15bd6545a61626b0eed4aaff8cd207134abfe38f
MartinStevko <mstevko10@gmail.com>
2018-04-15 19:35:46 +0200

    Product gallery

--- eshop/static/js/product_view.js
+++ eshop/static/js/product_view.js
@@ -54,11 +54,3 @@ function show_hide_section(i) {
     }
   }
 }
-
-function expand(imgs) {
-    var expandImg = document.getElementById("expandedImg");
-    var imgText = document.getElementById("imgtext");
-    expandImg.src = imgs.src;
-    imgText.innerHTML = imgs.alt;
-    expandImg.parentElement.style.display = "block";
-}
