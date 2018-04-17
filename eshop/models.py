1f8aca5da2ebe3d22731453cfd6f30de1e4d5057
MartinStevko <mstevko10@gmail.com>
2018-04-17 21:44:19 +0200

    Orders model creation

--- eshop/models.py
+++ eshop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from django.utils import timezone
+from random import randint
 
 class Division(models.Model):
     name = models.CharField(max_length=30, unique=True)
@@ -64,3 +65,26 @@ class Review(models.Model):
 
     def __str__(self):
         return "{} - {}".format(self.id, self.idProduct)
+
+class Order(models.Model):
+    serial_number = models.CharField(max_length=10, unique=True, default='0000000000')
+
+    products = models.ManyToManyField(Product)
+    amount = models.TextField()
+    price = models.IntegerField(default=0, unique=False)
+
+    getting = models.CharField(max_length=10, unique=False)
+
+    first_name = models.CharField(max_length=50, unique=False)
+    surname = models.CharField(max_length=50, unique=False)
+    phone = models.CharField(max_length=20, unique=False)
+
+    street = models.CharField(max_length=100, unique=False)
+    house = models.IntegerField(unique=False)
+    town = models.CharField(max_length=50, unique=False)
+    pdn = models.CharField(max_length=10, unique=False)
+
+    note = models.TextField()
+
+    def __str__(self):
+        return "Order {}".format(self.serial_number)
