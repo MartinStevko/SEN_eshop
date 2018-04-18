4c0613d498f9ca36408945468246f76299a659ed
MartinStevko <mstevko10@gmail.com>
2018-04-18 23:38:34 +0200

    Static and media files load when DEBUG = False

--- eshop_server/urls.py
+++ eshop_server/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('eshop/', include('eshop.urls', namespace='eshop')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 '''
 if settings.DEBUG is True:
