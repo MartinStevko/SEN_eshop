4f5ebed3386180a154f5adea4924c54cc408a1d5
MartinStevko <mstevko10@gmail.com>
2018-04-21 18:26:14 +0200

    Still same user function

--- eshop/static/js/product_view.js
+++ eshop/static/js/product_view.js
@@ -1,40 +1,69 @@
 var ids = {1:"1", 2:"2", 3:"3"};
 
+function still_same() {
+	var seconds = new Date().getTime() / 1000;
+
+	var time = localStorage.getItem("time");
+
+  localStorage.setItem("time", String(seconds));
+
+	if (time == null) {
+		time = 0;
+	}
+
+	var diff = seconds - time;
+
+	if (diff > 300) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
 function previous_style() {
-	var sec_styles = {1:null, 2:null, 3:null}
-  styles_in = true
+	var still = still_same();
 
-	for (i = 1; i < 4; i++) {
-    sec_styles[i] = localStorage.getItem(ids[i]);
+	if (still == true) {
+		var sec_styles = {1:null, 2:null, 3:null}
+	  styles_in = true
 
-    if (sec_styles[i] == null) {
-      styles_in = false;
-    }
-  }
+		for (i = 1; i < 4; i++) {
+	    sec_styles[i] = localStorage.getItem(ids[i]);
 
-  if (styles_in == true) {
-    for (i = 1; i < 4; i++) {
-      var sec = document.getElementById(ids[i]);
+	    if (sec_styles[i] == null) {
+	      styles_in = false;
+	    }
+	  }
 
-      if (sec.style.display !== sec_styles[i]) {
-        sec.style.display = sec_styles[i];
-      }
-    }
-  } else {
-    for (id in ids) {
-      var sec = document.getElementById(id);
+	  if (styles_in == true) {
+	    for (i = 1; i < 4; i++) {
+	      var sec = document.getElementById(ids[i]);
 
-      if (sec.style.display !== "none") {
-        sec.style.display = "none";
-      }
-    }
+	      if (sec.style.display !== sec_styles[i]) {
+	        sec.style.display = sec_styles[i];
+	      }
+	    }
+	  } else {
+	    for (id in ids) {
+	      var sec = document.getElementById(id);
 
-    var sec = document.getElementById("1");
-    sec.style.display = "block";
-  }
+	      if (sec.style.display !== "none") {
+	        sec.style.display = "none";
+	      }
+	    }
+
+	    var sec = document.getElementById("1");
+	    sec.style.display = "block";
+	  }
+	} else {
+		show_hide_section("1")
+	}
 }
 
 function show_hide_section(i) {
+	var seconds = new Date().getTime() / 1000;
+  localStorage.setItem("time", String(seconds));
+
   for (id in ids) {
     var sec = document.getElementById(id);
 
